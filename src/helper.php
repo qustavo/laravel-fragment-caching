c30d613ceea40d416734a5947ebf19af23b464f4
Thomas Broquist <thomas@happysprites.com>
2014-10-26 18:35:42 +0000

    Add an optional second parameter to cache, used to conditionaly enable/disable caching

--- src/helper.php
+++ src/helper.php
@@ -2,17 +2,19 @@
 
 if ( ! function_exists('cache') )
 {
-    function cache($key, Closure $closure)
+    function cache($key, $condition = true, Closure $closure)
     {
-        $content = Cache::get($key);
+        $content = $condition ? Cache::get($key) : false;
         if ( ! $content ) {
             ob_start();
             
             $closure();
             $content = ob_get_contents();
             ob_end_clean();
-            Cache::forever($key, $content);
-            Log::debug('writing cache', [$key]);
+	    if ($condition) {
+		    Cache::forever($key, $content);
+		    Log::debug('writing cache', [$key]);
+	    }
         } else {
             Log::debug('reading cache', [$key]);
         }
