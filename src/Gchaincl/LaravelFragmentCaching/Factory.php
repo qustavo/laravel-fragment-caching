6d353a18f147f8a700b9b87101ee13d504d4b132
Thomas Broquist <thomas@happysprites.com>
2014-11-02 08:04:00 +0000

    Fixed code duplication

--- src/Gchaincl/LaravelFragmentCaching/Factory.php
+++ src/Gchaincl/LaravelFragmentCaching/Factory.php
@@ -2,27 +2,6 @@
 
 class Factory extends \Illuminate\View\Factory {
 
-    public function cache($key, \Closure $closure)
-    {
-        $cache = $this->getContainer()['cache'];
-        $log = $this->getContainer()['log'];
-
-        $content = $cache->get($key);
-        if ( ! $content ) {
-            ob_start();
-
-            $closure();
-            $content = ob_get_contents();
-            ob_end_clean();
-            $cache->forever($key, $content);
-            $log->debug('writing cache', [$key]);
-        } else {
-            $log->debug('reading cache', [$key]);
-        }
-
-        return $content;
-    }
-
     public function cacheif($condition, $key, \Closure $closure)
     {
         if ( ! $condition ) {
@@ -46,4 +25,10 @@ class Factory extends \Illuminate\View\Factory {
 
         return $content;
     }
+
+    public function cache($key, \Closure $closure)
+    {
+        return $this->cacheif(true, $key, $closure);
+    }
+
 }
--- src/helper.php
+++ src/helper.php
@@ -1,26 +1,5 @@
 <?php
 
-if ( ! function_exists('cache') )
-{
-    function cache($key, Closure $closure)
-    {
-        $content = Cache::get($key);
-        if ( ! $content ) {
-            ob_start();
-            
-            $closure();
-            $content = ob_get_contents();
-            ob_end_clean();
-            Cache::forever($key, $content);
-            Log::debug('writing cache', [$key]);
-        } else {
-            Log::debug('reading cache', [$key]);
-        }
-
-        return $content;
-    }
-}
-
 if ( ! function_exists('cacheif') )
 {
     function cacheif($condition, $key, Closure $closure)
@@ -43,3 +22,13 @@ if ( ! function_exists('cacheif') )
         return $content;
     }
 }
+
+if ( ! function_exists('cache') )
+{
+    function cache($key, Closure $closure)
+    {
+        return cacheif(true, $key, $closure);
+    }
+}
+
+
